f305c4a2d21833704a7da5641f3772a41991bff6
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2025-01-20 07:28:20 -0300

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,6 @@
+''' Q3. Gera uma matriz de 3 números aleatórios uniformemente distribuídos no intervalo [0, 1) e 
+outra matriz de 3 números aleatórios normalmente distribuídos com média 0 e desvio padrão 1.'''
+
+import numpy as np
+print(np.random.rand(3))
+print(np.random.randn(3))
