97559a4946210756e94b1183691d8c3f0caedeef
João Ricardo <32642843+joaoricardao@users.noreply.github.com>
2025-03-09 10:24:07 -0300

    Update 2-matriz_3_por_4.py

--- 2-matriz_3_por_4.py
+++ 2-matriz_3_por_4.py
@@ -3,9 +3,9 @@ Petrus, 07/03/2025
 Criação de uma matriz para testes (3x4)
 '''
 
-from numpy import matrix
+import numpy as np 
 
-A = matrix([
+A = np.array([
             [1, 2, 3, 4],  
             [5, 6, 7, 8],
             [9, 10, 11, 12]
